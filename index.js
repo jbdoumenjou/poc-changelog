f3c1ef9eae38907e8904ccf9a2d22cd8989cde72
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2024-03-08 14:35:11 +0100

    fix: remove deprecated dependency

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const _ = require('lodash');
-
-function sum(numbers) {
-    return _.sum(numbers)
-}
-
 
 function helloWorld() {
     return "hello World"
 }
 
-module.exports = {
-    helloWorld,
-    sum
-}
+module.exports = helloWorld
